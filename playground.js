51771e4c9aead63a70d6ee85e9a490fff1ec62db
Jarbas Horst <jarbas_horst@hotmail.com>
2023-11-16 22:15:19 +0000

    Copilot fourth commit

--- playground.js
+++ playground.js
@@ -1,14 +1,9 @@
-//Write a JavaScript function to filter and return even numbers from a given list
-//Create a list of numbers from 1 to 10
-//Filter the list to only include even numbers
-//Print the filtered list to the console
+// Create a sample list of numbers
+// Create a list of even numbers from the sample list
+// return the list of even numbers
+// Example: [1, 2, 3, 4, 5, 6, 7, 8, 9] => [2, 4, 6, 8]
 
-function filterEvenNumbers(numbers) {
-    return numbers.filter(function (number) {
-      return number % 2 === 0;
-    });
-  }
-  
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const filteredNumbers = filterEvenNumbers(numbers);
-  console.log(filteredNumbers);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+console.log(evenNumbers);
+
